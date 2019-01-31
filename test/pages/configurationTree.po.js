55d4fcec2740d7e1625a61e1b124cb98309a928f
ICSC <ICSC@fundacion-jala.local>
2019-01-31 19:21:44 -0400

    updates

--- test/pages/configurationTree.po.js
+++ test/pages/configurationTree.po.js
@@ -3,7 +3,7 @@ const commonActions = require('../core/CommonActions');
 class ConfigurationTree {
 
     constructor() {
-        //todo use regular expresions
+        //todo use regular expressions in the url
         browser.url('https://na85.lightning.force.com/lightning/setup/SetupOneHome/home');
         this.configurationTree = 'div[data-aura-class="onesetupSetupNavTree"]'; //locator of tree view panel (configuration tree).
         this.searchField = 'input[data-interactive-lib-uid="3"]'; //locator of search field placed in the configurations tree pane.
